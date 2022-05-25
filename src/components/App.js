f71093e2eab44487ce6b29b09c6b0fd1d3ead394
Hrushi1509 <hrushikeshwalujkar@gmail.com>
2022-05-25 15:31:08 +0530

    Hrushi

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+// import Welcome from 'We'
 
 const App = () => {
   return (
-    <p>
-      Now I can render any React component on any DOM node I want using
-      ReactDOM.render
-    </p>
+    
+      <>
+        {/* ReactDOM.render(<p></p>)
+         */}
+         ReactDOM.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>)ReactDOM.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>)
+      </>
   );
 };
 
